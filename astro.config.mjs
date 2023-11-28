d37f02aebd192eb4c99dd25b28a7699667c82fd8
Tom Elliott <tomelliottnz@gmail.com>
2023-11-28 15:54:51 +1300

    config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tomelliott.co.nz",
+  base: "/nzsa-2023",
   integrations: [
     tailwind({
       applyBaseStyles: false,
